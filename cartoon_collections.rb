f653a1c2957ec1b8dc83c56b9880ebb22081a4fe
valeria campilongo <cvaleriac@hotmail.it>
2018-04-20 00:46:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |type|
       cheese_types.include?(type)
-    end 
+    end
   end
 end
